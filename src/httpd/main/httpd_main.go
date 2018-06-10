9711ad65d8e53475e418141a616c4d8ef399a708
Tim Wood <timwood0@pacbell.net>
2018-06-10 13:39:55 -0700

    Add missing launcher file!

---
+++ src/httpd/main/httpd_main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"httpd"
+	"net/http"
+	"log"
+)
+
+func main() {
+	addr := httpd.ProvideListeningAddr()
+	log.Println("Listening: " + addr)
+	echo := httpd.Echo{ addr }
+
+	http.Handle("/string", httpd.String(httpd.Always))
+	http.Handle("/echo", &echo)
+
+	err := http.ListenAndServe(addr + ":4000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
