226cc042088640b90754fa9790b55be949f89942
Tim Wood <timwood0@pacbell.net>
2018-06-12 20:27:00 -0700

    Force starting from install location.

--- go/start.sh
+++ go/start.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 # Start the Coursehero httpd.
 
-bindir=`dirname $0`
-
-# Enable starting from unpack or installed locations
 # See appspec.yml
-if [ -f "$bindir/bin/httpd" ]; then
-	chmod 755 "$bindir/bin/httpd"
-	$bindir/bin/httpd $1 &
-	pid=$!
-elif [ -f "$bindir/httpd" ]; then
-	chmod 755 "$bindir/httpd"
-	$bindir/httpd $1 &
-	pid=$!
-else
-	echo "Httpd not found."; exit 1
+bindir=/usr/local/coursehero/bin
+
+if [ ! -x "$bindir/httpd" ]; then
+	echo "$bindir/httpd not found."; exit 1
 fi
 
+$bindir/httpd $1 &
+pid=$!
 if [ -d /var/run/lock ]; then
 	echo $pid > /var/run/lock/httpd.pid
 fi
