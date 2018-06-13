d7aa1da8e520a7740b7e1141cd4e1ee3c92b81c2
Tim Wood <timwood0@pacbell.net>
2018-06-12 19:28:29 -0700

    Still searching for startup logic/CodeDeploy bug.

--- go/start.sh
+++ go/start.sh
@@ -2,8 +2,15 @@
 # Start the Coursehero httpd.
 
 bindir=`dirname $0`
-$bindir/httpd $1 &
-pid=$!
+if [ -x "$bindir/bin/httpd" ]; then
+	$bindir/bin/httpd $1 &
+	pid=$!
+elif [ -x "$bindir/httpd" ]; then
+	$bindir/httpd $1 &
+	pid=$!
+else
+	echo "Httpd not found."; exit 1
+fi
 
 if [ -d /var/run ]; then
 	echo $pid > /var/run/lock/httpd.pid
